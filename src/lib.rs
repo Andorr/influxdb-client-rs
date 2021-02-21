58ae58f8205f066788c5bf54604fa1220c20df3a
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2021-02-21 18:52:53 +0100

    ci: ran fmt and added workflows

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,41 @@
-
-pub mod models;
 pub mod client;
-
+pub mod models;
 
 #[cfg(test)]
 mod tests {
 
-    use super::client::{Client};
-    use super::models::{Point};
-    use mockito::{Matcher};
+    use super::client::Client;
+    use super::models::Point;
+    use mockito::Matcher;
 
     #[test]
     fn test_client_write() {
         let api_key = "TEST_API_KEY";
-
 
         let mock = mockito::mock("POST", "/api/v2/write")
             .with_status(201)
             .match_header("content-type", "text/plain")
             .match_query(Matcher::AllOf(vec![
                 Matcher::UrlEncoded("bucket".into(), "tradely".into()),
-                Matcher::UrlEncoded("orgID".into(), "168f31904923e853".into())
+                Matcher::UrlEncoded("orgID".into(), "168f31904923e853".into()),
             ]))
             .match_body("test,ticker=GME price=420.69 1613925577")
             .expect(1)
             .create();
 
-
-
-        let client = Client::new(
-            mockito::server_url(), 
-            String::from(api_key)
-        )
+        let client = Client::new(mockito::server_url(), String::from(api_key))
             .with_bucket("tradely")
             .with_org_id("168f31904923e853");
-        
 
         let point = Point::new("test")
             .tag("ticker", "GME")
             .field("price", 420.69)
             .timestamp(1613925577);
 
-
-        let points = vec![point]; 
+        let points = vec![point];
         let result = tokio_test::block_on(client.insert_points(&points));
-        
-        if let Err(err) = result {
-            println!("{:?}", err);
-            assert!(false);
-            return
-        } 
+
+        assert!(result.is_ok());
 
         mock.assert();
     }
--- src/models.rs
+++ src/models.rs
@@ -26,22 +26,20 @@ impl Value {
             Value::Str(s) => s.to_string(),
             Value::Int(i) => i.to_string(),
             Value::Float(f) => f.to_string(),
-            Value::Bool(b) => b.to_string(), 
+            Value::Bool(b) => b.to_string(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Point {
-
     pub measurement: String,
     pub timestamp: Option<i64>,
-    pub tags: Vec<(String, Value)>, 
+    pub tags: Vec<(String, Value)>,
     pub fields: Vec<(String, Value)>,
 }
 
 impl Point {
-
     pub fn new<T: Into<String>>(measurement: T) -> Self {
         Point {
             measurement: measurement.into(),
@@ -75,18 +73,24 @@ impl Point {
         // Write tags
         if !self.tags.is_empty() {
             write!(&mut builder, ",").unwrap();
-            
+
             for tag in self.tags {
                 write!(&mut builder, "{}={}", tag.0.to_string(), tag.1.to_string()).unwrap();
             }
         }
-        
+
         // Write fields
         if !self.fields.is_empty() {
             write!(&mut builder, " ").unwrap();
 
             for field in self.fields {
-                write!(&mut builder, "{}={}", field.0.to_string(), field.1.to_string()).unwrap();
+                write!(
+                    &mut builder,
+                    "{}={}",
+                    field.0.to_string(),
+                    field.1.to_string()
+                )
+                .unwrap();
             }
         }
 
@@ -116,13 +120,13 @@ pub enum InfluxError {
 }
 
 mod tests {
-    use super::{Point};
+    use super::Point;
 
     #[test]
     fn test_point_serialize() {
         let expected = "mem,host=host1 used_percent=23.43234543 1556896326";
 
-        let point  = Point::new("mem")
+        let point = Point::new("mem")
             .tag("host", "host1")
             .field("used_percent", 23.43234543)
             .timestamp(1556896326);
